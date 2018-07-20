ae327f62380bd56bf01fb78577642649dd6cfec9
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-07-20 09:39:30 +0200

    [MIG] account_cash_discount_base migration script
    Migration from the 8.0 branch.

---
+++ account_cash_discount_base/migrations/10.0.1.0.0/pre-migration.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openupgradelib import openupgrade
+
+
+def migrate(cr, version):
+    modules = [
+        ('account_cash_discount_payment_term', 'account_cash_discount_base'),
+    ]
+    openupgrade.update_module_names(cr, modules, merge_modules=True)
