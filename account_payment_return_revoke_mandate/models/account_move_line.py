884c080ed8c2a6c8ae901f2a7c94176b4b0b3db7
Quentin Groulard <quentin.groulard@acsone.eu>
2020-02-06 10:52:04 +0100

    [12.0] [ADD] Account payment return revoke mandate

---
+++ account_payment_return_revoke_mandate/models/account_move_line.py
@@ -0,0 +1,25 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+from odoo.exceptions import UserError
+
+
+class AccountMoveLine(models.Model):
+
+    _inherit = "account.move.line"
+
+    @api.multi
+    def _payment_returned(self, return_line):
+        super()._payment_returned(return_line)
+        if return_line.reason_id.revoke_mandates and self.mandate_id:
+            try:
+                self.mandate_id.cancel()
+                msg = (
+                    "Mandate revoked in payment return %s"
+                    % return_line.return_id.name
+                )
+                self.mandate_id.message_post(body=msg)
+            except UserError:
+                # May happen if the mandate is not in draft or valid state
+                pass
